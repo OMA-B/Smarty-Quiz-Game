edab910a6f5d05df2597779deb7ce67c9c84aa3e
Michael Adeshina <michaelking4christ@gmail.com>
2022-08-11 06:03:43 +0100

    almost done ...

--- questions/scoreboard/scoreboard.js
+++ questions/scoreboard/scoreboard.js
@@ -1,4 +1,26 @@
-// Answering question
+// Display Score
+const show_score = document.querySelector('.score span');
+// fetching stored score data
+let score = sessionStorage.getItem('scored');
+// appending score to display in percentage
+show_score.textContent = score * 25;
+
+// Dropdown Solutions Tab
+const drop_icon = document.querySelector('.fa-chevron-down');
+const quiz_container = document.querySelector('.quiz-container');
+// making quiz container open and close
+drop_icon.addEventListener('click', () => {
+    if (quiz_container.className !== 'open') {
+        quiz_container.className = 'open';
+        quiz_container.style.transition = 'all 1s';
+        drop_icon.style.transform = 'rotate(180deg)';
+    } else {
+        quiz_container.className = 'quiz-container';
+        drop_icon.style.transform = 'rotate(0deg)';
+    }
+});
+
+// Displaying Answers of Questions
 const options = document.querySelectorAll('.answer');
 const answers_database = ['Angel falls', 'Uranus', '1929', 'Hard to believe'];
 
